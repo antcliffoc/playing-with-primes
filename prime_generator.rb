44fc44f22e81594343f751cf8ef44c58b3d66b64
Oliver Antcliff <antcliffoc@gmail.com>
2019-08-02 12:28:44 +0200

    included user input to select range, and additional puts

--- prime_generator.rb
+++ prime_generator.rb
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env ruby
 def prime_generator(num)
   numbers = (3..num).to_a
   primes = [2,]
@@ -9,11 +9,15 @@ def prime_generator(num)
       is_prime = false
       break
       end}
-    if is_prime == true
-    primes.push(x)
-    end
+    primes.push(x) if is_prime == true
   }
+  primes.unshift(1)
   return primes
 end
-
-puts prime_generator(100000)
+puts "type a number to calculate the prime numbers in the range from 1 : x"
+number = gets.chomp.to_i
+puts "There are #{prime_generator(number).length} Prime numbers between 1 & #{number}"
+puts "The largest prime in this range is #{prime_generator(number)[-1]}"
+puts "show all the primes in this range? Y/N ?"
+user_response = gets.upcase!.chomp
+puts prime_generator(number) if user_response == "Y"
